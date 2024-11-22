86e300720985df1ced0d0a6f6590a067cfa4834e
CHARLES3321 <okekecharles363@gmail.com>
2024-11-22 14:29:36 +0100

    inserting

--- alx-react-app/App.jsx
+++ alx-react-app/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,4 +33,13 @@ function App() {
   )
 }
 
+
+function App()  {
+  return (
+    <div className='App'>
+      <WelcomeMessage />
+    </div>
+  );
+}
+ 
 export default App
